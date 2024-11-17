e797962b4a8e2d874e97ede80f3c470f9b89539a
Ayush Tyagi <ayush.tyagi145@gmail.com>
2024-11-17 22:06:21 +0530

    changed the timeline design

--- components/Homepage/Timeline.js
+++ components/Homepage/Timeline.js
@@ -1,4 +1,4 @@
-import React, { useRef, useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import { motion } from 'framer-motion';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import { Navigation } from 'swiper/modules'
@@ -42,9 +42,29 @@ const Timeline = () => {
     const prevButtonRef = useRef(null);
     const nextButtonRef = useRef(null);
 
+    const [isSmallScreen, setIsSmallScreen] = useState(false);
+
+    useEffect(() => {
+        // Check the screen width
+        const handleResize = () => {
+            setIsSmallScreen(window.innerWidth < 800);
+        };
+
+        // Set initial screen size
+        handleResize();
+
+        // Add event listener for window resize
+        window.addEventListener('resize', handleResize);
+
+        // Clean up the event listener on unmount
+        return () => {
+            window.removeEventListener('resize', handleResize);
+        };
+    }, []);
+
     return (
         <motion.div
-            className="mb-16 mt-24 lg:w-[75%] w-[90%] mx-auto"
+            className="mb-16 mt-24 lg:w-[80%] w-[95%] mx-auto"
             initial={{ opacity: 0 }}
             animate={{ opacity: 1 }}
             transition={{ duration: 0.8 }}
@@ -63,49 +83,70 @@ const Timeline = () => {
                 </p>
             </motion.div>
 
-            <div className="relative mt-10">
-                {/* Horizontal line in the middle */}
-                <div className="absolute left-0 right-0 top-1/2 transform -translate-y-1/2 h-[2px] bg-gray-300 z-0"></div>
-
-                <Swiper
-                    modules={[Navigation]}
-                    spaceBetween={20}
-                    slidesPerView={1.3}
-                    loop={false}
-                    onSlideChange={(swiper) => setActiveIndex(swiper.activeIndex)}
-                    navigation={{
-                        prevEl: prevButtonRef.current,
-                        nextEl: nextButtonRef.current,
-                    }}
-                    className="my-8"
-                >
-                    {TimelineData.map((phase, index) => (
-                        <SwiperSlide key={phase.id} className="relative my-2 ml-1">
-                            <div className="flex flex-col items-center py-6 bg-white shadow-md rounded-lg z-10">
-                                {/* About Company */}
-                                <div className="mb-4 text-left w-full px-6">
-                                    <h2 className="text-[22px] font-medium text-[#96034f] uppercase">{phase.niraliTitle}</h2>
-                                    <p className="text-sm font-light lg:w-[65%] mt-2">{phase.niraliContent}</p>
+            <div className="relative">
+                <div className='flex flex-row gap-5 items-center'>
+                    <div className='w-[30%] flex flex-col gap-5 h-[240px] justify-between'>
+                        <h1 className='-rotate-90 text-left text-[14px]'>We</h1>
+                        <h1 className=' justify-center text-[12px] lg:text-[16px] bg-[#96034f] text-white lg:px-4 px-2 text-center py-1 rounded-[8px] lg:rounded-[24px] uppercase'>Phase {activeIndex + 1}</h1>
+                        <h1 className='-rotate-90 text-right text-[14px]'>You</h1>
+                    </div>
+                    <Swiper
+                        modules={[Navigation]}
+                        spaceBetween={0}
+                        slidesPerView={isSmallScreen ? 1 : 1.2}
+                        loop={false}
+                        onSlideChange={(swiper) => setActiveIndex(swiper.activeIndex)}
+                        navigation={{
+                            prevEl: prevButtonRef.current,
+                            nextEl: nextButtonRef.current,
+                        }}
+                        className="my-8"
+                    >
+                        {TimelineData.map((phase, index) => (
+                            <SwiperSlide key={phase.id} className="relative lg:ml-4">
+                                <div className="flex flex-col items-center py-6 bg-white rounded-lg z-10">
+                                    {/* About Company */}
+                                    <div className="mb-4 text-left w-full">
+                                        <h2 className="text-[22px] font-medium text-[#96034f] uppercase">{phase.niraliTitle}</h2>
+                                        <p className="text-[13px] lg:text-[16px] font-light lg:w-[65%] mt-2">{phase.niraliContent}</p>
+                                    </div>
+                                    {/* Divider Line */}
+                                    <div className="w-full h-[2px] bg-black my-4 flex flex-row gap-10 items-center">
+                                        <span className='bg-[#fed9fe] py-1 px-3 rounded-full text-[14px]'>{index + 1}</span>
+                                        <img src="/assets/line.png" alt="Vertical line" />
+                                        <img src="/assets/line.png" alt="Vertical line" />
+                                        <img src="/assets/line.png" alt="Vertical line" />
+                                        <img src="/assets/line.png" alt="Vertical line" />
+                                        <img src="/assets/line.png" alt="Vertical line" />
+                                        <img src="/assets/line.png" alt="Vertical line" />
+                                        <img src="/assets/line.png" alt="Vertical line" />
+                                        <img src="/assets/line.png" alt="Vertical line" />
+                                        <img src="/assets/line.png" alt="Vertical line" />
+                                        <img src="/assets/line.png" alt="Vertical line" />
+                                        <img src="/assets/line.png" alt="Vertical line" />
+                                        <img src="/assets/line.png" alt="Vertical line" />
+                                        <img src="/assets/line.png" alt="Vertical line" />
+                                        <img src="/assets/line.png" alt="Vertical line" />
+                                        <img src="/assets/line.png" alt="Vertical line" />
+                                    </div>
+                                    {/* About Client */}
+                                    <div className='text-left w-full mt-4'>
+                                        <h2 className="text-[22px] font-medium text-gray-800 uppercase">{phase.clientTitle}</h2>
+                                        <p className="text-[13px] lg:text-[16px] font-light lg:w-[65%] mt-2">{phase.clientContent}</p>
+                                    </div>
                                 </div>
-                                {/* Divider Line */}
-                                <div className="w-full h-[2px] bg-gray-300 my-4"></div>
-                                {/* About Client */}
-                                <div className='text-left w-full px-6'>
-                                    <h2 className="text-[22px] font-medium text-gray-800 uppercase">{phase.clientTitle}</h2>
-                                    <p className="text-sm font-light lg:w-[65%] mt-2">{phase.clientContent}</p>
-                                </div>
-                            </div>
 
-                            {/* Blur Overlay for Peek of the Next Slide */}
-                            {index !== activeIndex && (
-                                <div className="absolute right-[-40px] top-0 h-full w-[110%] bg-white opacity-70 backdrop-blur-md"></div>
-                            )}
-                        </SwiperSlide>
-                    ))}
-                </Swiper>
+                                {/* Blur Overlay for Peek of the Next Slide */}
+                                {index !== activeIndex && (
+                                    <div className="absolute right-[-40px] top-0 h-full w-[105%] bg-white opacity-70 backdrop-blur-md"></div>
+                                )}
+                            </SwiperSlide>
+                        ))}
+                    </Swiper>
+                </div>
 
                 {/* Navigation Buttons Positioned at the Bottom Center */}
-                <div className="flex flex-row ml-4 gap-2">
+                <div className="flex flex-row ml-[8rem] gap-2">
                     <button
                         ref={prevButtonRef}
                         className={`${activeIndex === 0 && 'opacity-50'} custom-prev-button border-[1.5px] border-[#96034f] p-4 rounded-full ${activeIndex !== 0 && 'hover:bg-[#fed9fe]'} duration-300 transition`}
